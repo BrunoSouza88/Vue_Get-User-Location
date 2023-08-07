028858ff673f0888f374021cc0095884fb3d44c1
BrunoSouza <desouza88@hotmail.com>
2023-08-06 23:22:44 -0300

    feat: is possible to locate address and it shows on input

--- src/pages/UserLocation.vue
+++ src/pages/UserLocation.vue
@@ -6,59 +6,60 @@
         <div class="ui segment">
           <div class="field">
             <div class="ui right icon input larg">
-              <input type="text" placeholder="Enter your address">
+              <input type="text" placeholder="Enter your address" v-model="address">
               <i class="dot circle link icon" @click="locatorBtnPressed"></i>
             </div>
           </div>
         </div>
-        <button class="ui button">Go</button>
+        <button class="ui button">Search</button>
       </form>
     </div>
   </section>
 </template>
 
-
 <script>
 import axios from 'axios';
 
-const getAddressFrom = (lat, long) => {
-  const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
-  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat}, ${long}&key=${GOOGLE_API_KEY}`)
-    .then((response) => {
-      if (response.data.error_message) {
-        console.log(response.data.error_message);
-      } else {
-        console.log(response.data.results[0].formatted_address);
-      }
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
-};
-
-const getCurrentPosition = () => {
-  navigator.geolocation.getCurrentPosition((position) => {
-    getAddressFrom(position.coords.latitude, position.coords.longitude);
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-  },
-  (error) => {
-    alert(error.message);
-  });
-};
-
 export default {
+  data() {
+    return {
+      address: '',
+    };
+  },
   methods: {
     locatorBtnPressed() {
       if (navigator.geolocation) {
-        getCurrentPosition();
+        this.getCurrentPosition();
       } else {
         alert('Browser does not suport HTML Geolocalization');
       }
     },
+    getCurrentPosition() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.getAddressFrom(position.coords.latitude, position.coords.longitude);
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+      },
+      (error) => {
+        alert(error.message);
+      });
+    },
+    getAddressFrom(lat, long) {
+      const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+      axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat}, ${long}&key=${GOOGLE_API_KEY}`)
+        .then((response) => {
+          if (response.data.error_message) {
+            console.log(response.data.error_message);
+          } else {
+            this.address = response.data.results[0].formatted_address;
+          }
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    },
   },
 };
-
 </script>
 
 <style>
